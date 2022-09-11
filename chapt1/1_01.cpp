7b7ee89ef4d407cd13851f0980a6bd9dfe012949
CSDN-1999 <974353313@qq.com>
2022-09-11 15:59:20 +0800

    Create 1_01.cpp

---
+++ chapt1/1_01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+#include<string>
+
+int main()
+{
+    string first_name;
+    string last_name;
+    cout<<"请输入名字:";
+    cin>>first_name;
+    cout<<"请输入姓氏:";
+    cin>>last_name;
+
+    cout<<"他的名字为："<<last_name<<first_name<<endl;
+
+    return 0;
+}
